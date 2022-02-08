c131e54485acd1515a8ef6e5e1e2c6c8430af278
YuanYap <khaiyuanyap@gmail.com>
2022-02-08 16:09:59 +0800

    Update user.js

--- pages/user.js
+++ pages/user.js
@@ -91,7 +91,7 @@ export default function User() {
           </a>
         </div>
 
-        <div className="mb-3 flex flex-col items-center justify-center bg-nice-white py-2">
+        <div className="pb-3 flex flex-col items-center justify-center bg-nice-white py-2">
           <div className="container mx-auto px-9">
             <div className="grid gap-3 lg:grid-cols-4">
               {repos.map((repo) => (
