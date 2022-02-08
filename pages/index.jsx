cfd647d6b3a959c3bbb14811579de2c2a20d5cf5
YuanYap <khaiyuanyap@gmail.com>
2022-02-08 21:50:21 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,8 +13,10 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div>
+          <h1 className="text-3xl font-bold mb-2 text-center font-mono">GitHub Profile</h1>
+          <p className="text-md text-red-500 font-semibold mb-2 text-center font-mono">Heads up! There is a 60 times request limit.</p>
           <form
-            className="rounded-lg border border-gray-500 text-center font-mono  shadow-xl outline-0 active:border-gray-600	"
+            className="rounded-lg bg-white border border-gray-500 text-center font-mono  shadow-xl outline-0 active:border-gray-600	"
             onSubmit={(e) => {
               e.preventDefault()
               Router.push({
