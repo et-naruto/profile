e1f7b9097d5366f530d7aa188375d761107ed0b1
YuanYap <khaiyuanyap@gmail.com>
2022-02-09 12:26:43 +0800

    UI

--- pages/user.js
+++ pages/user.js
@@ -165,10 +165,10 @@ export default function User() {
 
         <div className="flex flex-col items-center justify-center bg-nice-white pb-10">
           <div className="container mx-auto px-6">
-            <div className="grid gap-1 md:gap-3 lg:grid-cols-4">
+            <div className="grid md:gap-3 lg:grid-cols-4">
               {repos.map((repo) => (
                 <div
-                  className="delay-30 mt-8 cursor-pointer rounded-xl border bg-white p-6 transition duration-100 ease-in-out hover:shadow-lg"
+                  className="delay-30 mt-8 cursor-pointer rounded-xl border bg-white p-6 transition duration-100 ease-in-out shadow-md hover:shadow-xl hover:-translate-y-0.5 hover:border-gray-800"
                   key={repo.id}
                 >
                   <a href={repo.html_url}>
